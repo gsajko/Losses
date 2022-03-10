00ba57d6be7a55c475b02daefe8bc4c0e3cd3d61
gsajko <grzegorz.sajko@protonmail.com>
2022-03-10 10:19:42 +0100

    update

--- to_spreadsheet.py
+++ to_spreadsheet.py
@@ -2,22 +2,24 @@
 import os
 import pandas as pd
 # %%
-df = pd.read_table("losses_table.md", sep="|").iloc[1:]
-# %%
-# df.drop(['C', 'D'], axis = 1)
-for col in df.columns:
-    if "Unnamed" in col:
-        df.drop(col, axis=1, inplace=True)
-df
-# %%
-df.to_csv("losses_table.csv")
+# df = pd.read_table("losses_table.md", sep="|").iloc[1:]
+# # %%
+# # df.drop(['C', 'D'], axis = 1)
+# for col in df.columns:
+#     if "Unnamed" in col:
+#         df.drop(col, axis=1, inplace=True)
+# df
+# # %%
+# df.to_csv("losses_table.csv")
 
 # %%
 import gspread
 # %%
-gc = gspread.service_account()
+SERVICE_ACC = os.environ["SERVICE_ACC"]
+gc = gspread.service_account(SERVICE_ACC)
+print("done❗️")
+# gc = gspread.service_account("config/auth.json")
 # %%
 # auth_path: str = "config/auth.json"
 # auth = json.load(open(auth_path))
 # TOKEN = auth["token"]
-TOKEN = os.environ["TOKEN"]
