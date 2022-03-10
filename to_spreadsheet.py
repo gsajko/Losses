3f21200169e821ef64ea05c507debf1c39f8ed0a
gsajko <grzegorz.sajko@protonmail.com>
2022-03-10 16:03:54 +0100

    created integration with google spreadsheet

--- to_spreadsheet.py
+++ to_spreadsheet.py
@@ -1,21 +1,45 @@
 # %%
 import os
 import pandas as pd
-from pathlib import Path
-# %%
-# df = pd.read_table("losses_table.md", sep="|").iloc[1:]
-# # %%
-# # df.drop(['C', 'D'], axis = 1)
-# for col in df.columns:
-#     if "Unnamed" in col:
-#         df.drop(col, axis=1, inplace=True)
-# df
-# # %%
-# df.to_csv("losses_table.csv")
-
-# %%
 import gspread
-# gc = gspread.service_account("config/auth.json")
+from pathlib import Path
+import gspread_pandas
+# %%
+# convert md to df and csv
+df = pd.read_table("losses_table.md", sep="|").iloc[1:]
+for col in df.columns:
+    if "Unnamed" in col:
+        df.drop(col, axis=1, inplace=True)
+df.to_csv("losses_table.csv")
+# %%
+# send df to google sheet
+file_path = Path.home().joinpath("secrets/auth.json")
+gs_config = gspread_pandas.conf.get_config(file_name=file_path)
+spread = gspread_pandas.Spread('Russian Losses', config=gs_config)
+spread.df_to_sheet(df, replace=True, index=False)
+# %%
+# format first column
 file_path = Path.home().joinpath("secrets/auth.json")
 gc = gspread.service_account(file_path)
-print("done❗️")
+sh = gc.open('Russian Losses')
+sheetId = 0
+body = {
+    "requests": [
+        {
+            "updateDimensionProperties": {
+                "range": {
+                    "sheetId": sheetId,
+                    "dimension": "COLUMNS",
+                    "startIndex": 0,
+                    "endIndex": 1
+                },
+                "properties": {
+                    "pixelSize": 500
+                },
+                "fields": "pixelSize"
+            }
+        }
+    ]
+}
+res = sh.batch_update(body)
+# %%
