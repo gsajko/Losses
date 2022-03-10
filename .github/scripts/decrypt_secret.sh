f5549bf50cdf8e4aa9482a9d0346faeebcb244ec
gsajko <grzegorz.sajko@protonmail.com>
2022-03-10 10:49:57 +0100

    update

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/auth.json auth.json.gpg
+--output $HOME/secrets/auth.json config/auth.json.gpg
