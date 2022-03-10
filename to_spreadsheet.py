705bda950e2061468deebc4c39b7272fad1f516c
gsajko <grzegorz.sajko@protonmail.com>
2022-03-10 17:09:30 +0100

    formatting

--- to_spreadsheet.py
+++ to_spreadsheet.py
@@ -4,6 +4,7 @@ import pandas as pd
 import gspread
 from pathlib import Path
 import gspread_pandas
+
 # %%
 # convert md to df and csv
 df = pd.read_table("losses_table.md", sep="|").iloc[1:]
@@ -14,17 +15,24 @@ df.to_csv("losses_table.csv", index=False)
 # %%
 # send df to google sheet
 file_path = Path.home().joinpath("secrets/auth.json")
+
+gc = gspread.service_account(file_path)
+
+sh = gc.open("Russian Losses")
+worksheet = sh.worksheet("Sheet1")
+sheetId = 0
+# first remove old spreadsheet
+worksheet.clear()
+
+
 gs_config = gspread_pandas.conf.get_config(file_name=file_path)
-spread = gspread_pandas.Spread('Russian Losses', config=gs_config)
+spread = gspread_pandas.Spread("Russian Losses", config=gs_config)
 spread.df_to_sheet(df, replace=True, index=False)
 # %%
-# format first column
-file_path = Path.home().joinpath("secrets/auth.json")
-gc = gspread.service_account(file_path)
-sh = gc.open('Russian Losses')
-sheetId = 0
+
 # %%
-ranges = [[0,1,500],[1,10, 100]]
+# formmating
+ranges = [[0, 1, 500], [1, 10, 100]]
 for columns in ranges:
     body = {
         "requests": [
@@ -39,11 +47,33 @@ for columns in ranges:
                     "properties": {
                         "pixelSize": columns[2],
                     },
-                    "fields": "pixelSize"
+                    "fields": "pixelSize",
                 }
             }
         ]
     }
     res = sh.batch_update(body)
 
+worksheet.format("A1:F1", {"textFormat": {"bold": True}})
+worksheet.format("A2:F99", {"textFormat": {"bold": False}})
+worksheet.format("A1:A1", {"horizontalAlignment": "LEFT"})
+worksheet.format(
+    "F1:F40",
+    {
+        "textFormat": {
+            "bold": True,
+        },
+        # "backgroundColor": {"red": 1.0, "green": 1.0, "blue": 0.56},
+    },
+)
+# %%
+# add source
+worksheet = sh.worksheet("Sheet1")
+rows = [
+    ".",
+    "source:",
+    "https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-equipment.html",
+]
+for row in rows:
+    worksheet.append_row([row], table_range="B2:D4")
 # %%
