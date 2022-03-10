043e03c794ead4fa8c9f5be60bd24cedb2d3441d
gsajko <grzegorz.sajko@protonmail.com>
2022-03-10 11:51:11 +0100

    Squashed commit of the following:
    commit c03206d0dea80ecf9af145bff6d97655505316ea
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 11:48:25 2022 +0100
    
        update
    
    commit da2795b31d939526a11057e336372b8c481d3879
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 11:44:19 2022 +0100
    
        update
    
    commit 05b3890a5e7d72c30e49b9159b499cf539de6f14
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 11:31:10 2022 +0100
    
        update
    
    commit b40bc5bba9c1baa0eacf86418bc5df764e1fdcd5
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 11:29:31 2022 +0100
    
        update
    
    commit a872bf37656f5d560a7522766b4a30b71d4e083b
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 11:18:41 2022 +0100
    
        update
    
    commit 74288b438bbc2c0561d0028d777da5c5b7c4b09f
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 11:11:59 2022 +0100
    
        update
    
    commit 009bc5b1b81f6143c13eb9f77a7092fc538206c3
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 11:02:52 2022 +0100
    
        update
    
    commit a4f6c3aeb6fd4ed25a19b96f89d92b8dafce3f5e
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 11:00:59 2022 +0100
    
        update
    
    commit f5549bf50cdf8e4aa9482a9d0346faeebcb244ec
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 10:49:57 2022 +0100
    
        update
    
    commit 61e496e6553e3c6c3ce47d382b6a1b4b5c66cd9d
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 10:47:10 2022 +0100
    
        update
    
    commit 85be08cb3b6b15f4a172de3bbeccac0f643330f2
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 10:43:31 2022 +0100
    
        update
    
    commit 5f4b703de8f02d314354c8e0ef97631d61ad92ea
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 10:42:45 2022 +0100
    
        update
    
    commit a1d934f9ae00938319f1a98c761a49e154b36ed0
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 10:36:00 2022 +0100
    
        Add new decryption script
    
    commit 0c6db3693361650d1b71568dc7dee8f264c4fa7d
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 10:28:00 2022 +0100
    
        update
    
    commit 00ba57d6be7a55c475b02daefe8bc4c0e3cd3d61
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 10:19:42 2022 +0100
    
        update
    
    commit 9bb499ac1022f5a147ae7bec6b9aa1ed38c5783d
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 10:02:40 2022 +0100
    
        update
    
    commit 12df3f43ce9ceb40aef2a66c4123c3e95ead8f44
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 09:59:53 2022 +0100
    
        update
    
    commit 5a059533c42370d8c81c6f8beb357ac6a24483b0
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 09:58:27 2022 +0100
    
        update
    
    commit 0d20ea53b917e5a4f03ad83957a07b07d62f9219
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 09:51:05 2022 +0100
    
        update
    
    commit 3d4fa806189d462f707b417c320fe354586d6dc5
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 09:48:07 2022 +0100
    
        update
    
    commit b7a86ab2bad3863029f52bf51164a0a614f844a4
    Author: gsajko <grzegorz.sajko@protonmail.com>
    Date:   Thu Mar 10 09:43:18 2022 +0100
    
        update

--- to_spreadsheet.py
+++ to_spreadsheet.py
@@ -1,23 +1,21 @@
 # %%
 import os
 import pandas as pd
+from pathlib import Path
 # %%
-df = pd.read_table("losses_table.md", sep="|").iloc[1:]
-# %%
-# df.drop(['C', 'D'], axis = 1)
-for col in df.columns:
-    if "Unnamed" in col:
-        df.drop(col, axis=1, inplace=True)
-df
-# %%
-df.to_csv("losses_table.csv")
+# df = pd.read_table("losses_table.md", sep="|").iloc[1:]
+# # %%
+# # df.drop(['C', 'D'], axis = 1)
+# for col in df.columns:
+#     if "Unnamed" in col:
+#         df.drop(col, axis=1, inplace=True)
+# df
+# # %%
+# df.to_csv("losses_table.csv")
 
 # %%
 import gspread
-# %%
-gc = gspread.service_account()
-# %%
-# auth_path: str = "config/auth.json"
-# auth = json.load(open(auth_path))
-# TOKEN = auth["token"]
-TOKEN = os.environ["TOKEN"]
+# gc = gspread.service_account("config/auth.json")
+file_path = Path.home().joinpath("secrets/auth.json")
+gc = gspread.service_account(file_path)
+print("done❗️")
